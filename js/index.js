668762fd3d922473d12878a276b8e42cd0ebf77e
macaperez <macaperezcarrillo@gmail.com>
2018-01-30 22:56:37 +0000

    JS COOKIES + PRIVACI-POLICY.HTML cambios en CSS

--- js/index.js
+++ js/index.js
@@ -1,4 +1,16 @@
 $(document).ready(function() {
+  $.cookieBar({
+    fixed: true,
+    bottom: true,
+    zindex: 10,
+    message:'Este sitio web utiliza cookies, tanto propias como de terceros para recopilar información estadística sobre su navegación y mostrarle publicidad relacionada con sus preferencias, generada a partir de sus pautas de navegación. Si continua navegando, consideramos que acepta su uso.',
+    acceptText: 'Acepto',
+    policyButton: true,
+    policyText: 'Más info sobre política de privacidad',
+    policyURL: 'pages/privacy-policy.html',
+    declineText: 'No estoy de acuerdo',
+    declineButton: true
+  });
   $('#fullpage').fullpage({});
   $('.testimonios').slick({
     autoplay: true,
@@ -6,7 +18,7 @@ $(document).ready(function() {
   });
   $('.recetas-foto').slick({
     autoplay: true,
-    autoplaySpeed: 4500,
+    autoplaySpeed: 3100,
     fade: true,
     cssEase: 'linear'
   });
